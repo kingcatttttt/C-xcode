fcbe2ecde027d96f6de6c677dad1b211c8b615b0
kingcatttttt <ssvyat7007gmail.com>
2023-10-18 23:06:28 +0200

    first test

---
+++ index/index/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  index
+//
+//  Created by Sviatoslav on 18.10.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
